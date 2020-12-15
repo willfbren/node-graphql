15c92f47f66fc248f18e3d916afbe02f3da4cd73
willfbren <willfbren@gmail.com>
2020-12-14 23:00:14 -0600

    connected primsa and graphql

--- src/index.js
+++ src/index.js
@@ -1,31 +1,26 @@
 const { ApolloServer } = require('apollo-server');
+const { PrismaClient } = require('@prisma/client');
 const fs = require('fs');
 const path = require('path');
 
-let links = [
-    {
-        id: 'link-0',
-        url: 'www.howtographql.com',
-        description: 'Fullstack tutorial for GraphQL',
-    },
-];
-
-let idCount = links.length;
+const prisma = new PrismaClient();
 
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
-        feed: () => links,
+        feed: async (parent, args, context) => {
+            return context.prisma.link.findMany();
+        },
     },
     Mutation: {
-        post: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url,
-            };
-            links.push(link);
-            return link;
+        post: (parent, args, context, info) => {
+            const newLink = context.prisma.link.create({
+                data: {
+                    url: args.url,
+                    description: args.description,
+                },
+            });
+            return newLink;
         },
     },
 };
@@ -36,6 +31,9 @@ const server = new ApolloServer({
         'utf-8'
     ),
     resolvers,
+    context: {
+        prisma,
+    },
 });
 
 server
